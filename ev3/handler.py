5ec195b870677c9c26d98cbcaa6885b320e10f28
Jaakko Suutarla <jaakko@suutarla.com>
2017-10-02 20:32:41 +0300

    Fix stupid mistake

--- ev3/handler.py
+++ ev3/handler.py
@@ -6,11 +6,11 @@ if platform == "linux" or platform == "linux2":
   
 def start(emit):
   if platform == "linux" or platform == "linux2":
-    goless.go(start_sensors, emit)
+    start_sensors(emit)
   else: 
     print("not working")
 
-def start_sensors(emit):
+def start_sensors():
   brick = brickpi3.BrickPi3()
   color_readings = goless.chan()
   infrared_readings = goless.chan()
@@ -19,6 +19,8 @@ def start_sensors(emit):
   start_touch_sensor(brick, brick.PORT_1, touch_readings)
   start_color_sensor(brick, brick.PORT_2, color_readings)
   start_infrared_sensor(brick, brick.PORT_3, infrared_readings)
+
+  goless.go(start_sensors, emit, color_readings, infrared_readings, touch_readings)
 
 
 def start_emitter(emit, color_readings, infrared_readings, touch_readings):
